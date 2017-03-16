c26e788ad17291ca9274fbe533e77e423f4e7fa4
System <none>
2017-03-16 04:17:48 +0000

    update for diff comparison to see what broke :(

--- bot.js
+++ bot.js
@@ -52,8 +52,6 @@ app.post('/webhook', function (req, res) {
       entry.messaging.forEach(function(event) {
         if (event.message) {
           receivedMessage(event);
-        } else if (event.postback) {
-          receivedPostback(event);   
         } else {
           console.log("Webhook received unknown event: ", event);
         }
@@ -76,8 +74,8 @@ function receivedMessage(event) {
   var timeOfMessage = event.timestamp;
   var message = event.message;
 
-  console.log("Received message for user %d and page %d at %d with message:", 
-    senderID, recipientID, timeOfMessage);
+  console.log("Received message from user %d with message:", 
+    senderID);
   console.log(JSON.stringify(message));
 
   var messageId = message.mid;
@@ -89,43 +87,25 @@ function receivedMessage(event) {
     // If we receive a text message, check to see if it matches a keyword
     // and send back the template example. Otherwise, just echo the text we received.
     switch (messageText) {
-      case 'generic':
-        sendGenericMessage(senderID);
-        break;
-
       case 'meme':
         sendMeme(senderID);
         break;
 
       default:
-        sendTextMessage(senderID, messageText);
+        sendApiDescription(senderID);
     }
   } else if (messageAttachments) {
-    sendTextMessage(senderID, "Message with attachment received");
+    //sendTextMessage(senderID, "Message with attachment received");
   }
-}
-
-function receivedPostback(event) {
-  var senderID = event.sender.id;
-  var recipientID = event.recipient.id;
-  var timeOfPostback = event.timestamp;
-
-  // The 'payload' param is a developer-defined field which is set in a postback 
-  // button for Structured Messages. 
-  var payload = event.postback.payload;
-
-  console.log("Received postback for user %d and page %d with payload '%s' " + 
-    "at %d", senderID, recipientID, payload, timeOfPostback);
-
-  // When a postback is called, we'll send a message back to the sender to 
-  // let them know it was successful
-  sendTextMessage(senderID, "Postback called");
 }
 
 //////////////////////////
 // Sending helpers
 //////////////////////////
+
 function sendTextMessage(recipientId, messageText) {
+  request()
+  
   var messageData = {
     recipient: {
       id: recipientId
@@ -138,55 +118,30 @@ function sendTextMessage(recipientId, messageText) {
   callSendAPI(messageData);
 }
 
-function sendGenericMessage(recipientId) {
-  var messageData = {
-    recipient: {
-      id: recipientId
-    },
-    message: {
-      attachment: {
-        type: "template",
-        payload: {
-          template_type: "generic",
-          elements: [{
-            title: "rift",
-            subtitle: "Next-generation virtual reality",
-            item_url: "https://www.oculus.com/en-us/rift/",               
-            image_url: "http://messengerdemo.parseapp.com/img/rift.png",
-            buttons: [{
-              type: "web_url",
-              url: "https://www.oculus.com/en-us/rift/",
-              title: "Open Web URL"
-            }, {
-              type: "postback",
-              title: "Call Postback",
-              payload: "Payload for first bubble",
-            }],
-          }, {
-            title: "touch",
-            subtitle: "Your Hands, Now in VR",
-            item_url: "https://www.oculus.com/en-us/touch/",               
-            image_url: "http://messengerdemo.parseapp.com/img/touch.png",
-            buttons: [{
-              type: "web_url",
-              url: "https://www.oculus.com/en-us/touch/",
-              title: "Open Web URL"
-            }, {
-              type: "postback",
-              title: "Call Postback",
-              payload: "Payload for second bubble",
-            }]
-          }]
-        }
-      }
-    }
-  };  
-
-  callSendAPI(messageData);
-}
-
 function logObject(obj) {
   console.log(JSON.stringify(obj, null, 2));
+}
+
+function sendApiDescription(recipientId) {
+  sendTextMessage(recipientId, "sssss");
+  return;
+  request({
+      url: 'https://graph.facebook.com/v2.6/' + recipientId 
+        + '?access_token=' + process.env.PAGE_ACCESS_TOKEN
+    },
+    function (error, response, body) {
+      if (error || response.statusCode != 200) return;
+    
+      var fbProfileBody = JSON.parse(body);
+      var userName = fbProfileBody["first_name"];
+      var apiDesc = `Hello ${userName}, 
+        I'm your personal memebot! ¯\_(ツ)_/¯
+        type 'meme' and see what happens...
+        (type help for more details)
+      `;
+      sendTextMessage(recipientId, "sssss");
+    }
+  );
 }
 
 function sendMeme(recipientId) {
