ffa4727202e896ae41b79589facbbbec38fb0d9e
Ben Winding <ben.winding@gmail.com>
2018-05-03 11:25:07 +0930

    Fix share command

--- repositories/messenger.js
+++ repositories/messenger.js
@@ -39,8 +39,8 @@ exports.SendShareMe = (recipientId) => {
         payload:{
           template_type:"generic",
           elements:[{
-            title:"Click here ↑ to talk to Meme Bot",
-            subtitle:"... Or click here ↓ to send Meme Bot to your friends",
+            title:"↑↑ Click to talk to Meme Bot!",
+            subtitle:"↓↓ Send Meme Bot to your friends!",
             image_url:"https://i.imgur.com/RJSjTF6.jpg",
             item_url: "https://m.me/1memebot",
             buttons:[{
